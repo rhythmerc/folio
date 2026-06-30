562df5afe089178c5970e76a371b3a89ca86efeb
rhythmerc <ryandavidmercado@gmail.com>
2026-06-30 16:13:32 -0400

    swap to 2bit for builtin fonts

--- lib/EpdFont/scripts/convert-builtin-fonts.sh
+++ lib/EpdFont/scripts/convert-builtin-fonts.sh
@@ -28,7 +28,7 @@ for size in ${LITERATA_FONT_SIZES[@]}; do
     font_name="literata_${size}_$(echo $style | tr '[:upper:]' '[:lower:]')"
     font_path="../builtinFonts/source/Literata/Literata-${style}.ttf"
     output_path="../builtinFonts/${font_name}.h"
-    python fontconvert.py $font_name $size $font_path $(maybe_hebrew literata $size $style) --compress --pnum > $output_path
+    python fontconvert.py $font_name $size $font_path $(maybe_hebrew literata $size $style) --2bit --compress --pnum > $output_path
     echo "Generated $output_path"
   done
 done
@@ -38,7 +38,7 @@ for size in ${NOTOSANS_FONT_SIZES[@]}; do
     font_name="notosans_${size}_$(echo $style | tr '[:upper:]' '[:lower:]')"
     font_path="../builtinFonts/source/NotoSans/NotoSans-${style}.ttf"
     output_path="../builtinFonts/${font_name}.h"
-    python fontconvert.py $font_name $size $font_path --compress --pnum > $output_path
+    python fontconvert.py $font_name $size $font_path --2bit --compress --pnum > $output_path
     echo "Generated $output_path"
   done
 done
@@ -46,7 +46,7 @@ done
 python fontconvert.py notosans_8_regular 8 \
   ../builtinFonts/source/NotoSans/NotoSans-Regular.ttf \
   ../builtinFonts/source/NotoSansHebrew/NotoSansHebrew-Regular.ttf \
-  --additional-intervals 0x05D0,0x05EA > ../builtinFonts/notosans_8_regular.h
+  --additional-intervals 0x05D0,0x05EA --2bit > ../builtinFonts/notosans_8_regular.h
 
 echo ""
 echo "Running compression verification..."
